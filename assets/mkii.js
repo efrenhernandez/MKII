a5b0bdc94aff224ea611a11e4ec73504686bb506
Isaac Hernandez <isaac.hho@gmail.com>
2014-10-18 12:16:39 -0700

    Fixes #6 Adds a gameloop

--- assets/mkii.js
+++ assets/mkii.js
@@ -1,27 +1,39 @@
-// when page opens
+// canvas info
+var canvas; var ctx; var w; var h;
+// background info
+var bg; var bgX; var bgY;
+//foreground info
+var fg; var fgX; var fgY;
+
+// set everything when page opens
 $(document).ready(function () {
   // Set up canvas
-  var canvas = $("#canvas")[0];
-  var ctx = canvas.getContext("2d");
-  var w = $("#canvas").width();
-  var h = $("#canvas").height();
-
+  canvas = $("#canvas")[0];
+  ctx = canvas.getContext("2d");
+  w = $("#canvas").width();
+  h = $("#canvas").height();
   // set the canvas border
   ctx.StrokeStyle = "black";
   ctx.strokeRect(0, 0, w, h);
 
-  // background
-  var bg = new Image();
-  bg.onload = function() {
-    ctx.drawImage(bg, 0, 0);
-  };
+  // set background
+  bg = new Image();
+  bgX = 0; bgY = 0;
   bg.src = "assets/imgs/test_background.jpg";
-
-  // foreground
-  var fg = new Image();
-  fg.onload = function() {
-    ctx.drawImage(fg, 0, 400);
-  };
+  // set foreground
+  fg = new Image();
+  fgX = 0; fgY = 350;
   fg.src = "assets/imgs/test_background.jpg";
 });
 
+// loop (30 FPS)
+setInterval(function() { update(); draw(); }, 1000/30);
+
+function update() {
+  
+}
+
+function draw() {
+  ctx.drawImage(bg, bgX, bgY);
+  ctx.drawImage(fg, fgX, fgY);
+}
